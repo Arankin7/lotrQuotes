23006caeeb2360a18dfd4d3792efdba4cb6d8e42
Arankin7 <30415670+Arankin7@users.noreply.github.com>
2023-01-04 13:52:49 -0500

    pulls quotes from movies along with the character that said the quote

--- src/components/Quotes/index.js
+++ src/components/Quotes/index.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 function Quotes() {
 
+    const [quote, setQuote] = useState();
+    const [character, setCharacter] = useState();
+
+    useEffect(() => {
+        const headers = {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer gVBU6pctBRCL5GWwQ-4v'
+        }
+
+        const fetchData = async () => {
+            const rawData = await fetch('https://the-one-api.dev/v2/quote', {
+                headers: headers
+            })
+
+            const quotes = await rawData.json();
+            // Pulls a random quote
+            const quote = quotes.docs[Math.floor(Math.random() * quotes.docs.length)];
+            setQuote(quote.dialog);
+
+            const rawCharacters = await fetch('https://the-one-api.dev/v2/character?_id=' + quote.character, {headers: headers});
+            const characters = await rawCharacters.json();
+            const character = characters.docs[0];
+            setCharacter(character.name)
+        };
+
+        
+
+        fetchData();
+    }, []);
+
     return (
         <div>
-            My quote.
+            <div> {quote} </div>
+            <div> - {character} </div>
         </div>
     )
 }
